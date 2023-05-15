36e1ceef46e676a4bafafab9ca14cdfec7722d22
Pedro Lucas <pedrolucasmoraisborges@gmail.com>
2023-05-15 20:56:51 -0300

    teste

--- templates/seus_bicos.php
+++ templates/seus_bicos.php
@@ -42,82 +42,6 @@ $caminho = 'http://localhost/BicoJobs/';
         
         <button class="botao_pesquisa"><img src="<?php echo $caminho."media/svg's/search.svg"?>" alt="Lupa"></button>
 
-        <!--
-        <div class="filtro">
-            <button class="botao_filtro" onclick="abrir_filtro()"><img src="/media/svg's/filtro.svg" alt="filtro"></button>
-        
-            <div class="campo_filtro none">
-                <button onclick="fechar_filtro()" class="fechar"><img src="/media/svg's/x.svg" alt="X"></button>
-
-                <div class="options">
-                    <div class="area">
-                        <h3>Área de autação:</h3>
-                        <div>
-                            <input type="radio" name="area_atuacao" id="limpeza">
-                            <label for="limpeza">Limpeza</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="area_atuacao" id="construcao">
-                            <label for="construcao">Construção</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="area_atuacao" id="encanamento">
-                            <label for="encanamento">Encanamento</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="area_atuacao" id="eletrica">
-                            <label for="eletrica">Elétrica</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="area_atuacao" id="educacao">
-                            <label for="educacao">Educação</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="area_atuacao" id="alimentacao">
-                            <label for="alimentacao">Alimentação</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="area_atuacao" id="digital">
-                            <label for="digital">Digital</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="area_atuacao" id="cuidados">
-                            <label for="cuidados">Cuidados</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="area_atuacao" id="mecanico">
-                            <label for="mecanico">Mecânico</label>
-                        </div>
-                    </div>
-
-
-                    <div class="valor">
-                        <h3>Valor:</h3>
-                        <div>
-                            <input type="radio" name="valor" id="zero_cinquenta">
-                            <label for="zero_cinquenta">De R$ 00.00 a R$ 50.00</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="valor" id="cinquenta_cem">
-                            <label for="cinquenta_cem">De R$ 50.01 a R$ 100.00</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="valor" id="cem_acima">
-                            <label for="cem_acima">Acima de R$ 100.00</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="valor" id="a_combinar">
-                            <label for="a_combinar">A combinar</label>
-                        </div>
-                    </div>
-                </div>
-
-                <button class="aplicar" onclick="fechar_filtro()">Aplicar</button>
-            </div>
-        </div>
-        -->
-
-
         <button class="adicionar" onclick="adicionar()">
             <img src="<?php echo $caminho."media/svg's/plus.svg"?>" alt="Adicionar">
             <p>Anunciar o seu serviço</p>
@@ -135,6 +59,19 @@ $caminho = 'http://localhost/BicoJobs/';
 
         if($result->num_rows > 0){
             while($row = $result->fetch_assoc()){
+                $nome_comp_ofertante = $_SESSION['nome_comp'];
+                $avaliacao = $_SESSION['avaliacao'];
+                $nome_user_ofertante = $_SESSION['nome'];
+
+                $nome = $row['nome'];
+                $descricao = $row['descricao'];
+                $horario = $row['horario'];
+                $img_fundo = $row['img_servico'];
+                $valor = $row['valor'];
+                if($valor == 0.0){
+                    $valor = "A combinar";
+                }
+
                 echo "<div class='card'>
             <img src='$caminho/media/limp.svg' alt='#' class='img_fundo'>
 
@@ -145,14 +82,14 @@ $caminho = 'http://localhost/BicoJobs/';
 
                 <div class='info_princ'>
                     <img src='$caminho/media/area-atuação/limpeza.svg' alt=''>
-                    <h2>Faxina</h2>
+                    <h2>$nome</h2>
                 </div>
                 
 
                 <div class='info_sec'>
-                    <p><strong>Horário:</strong> Manhã/Tarde</p>
-                    <p><strong>Valor:</strong> A combinar</p>
-                    <p><strong>Pedro</strong> 3.0</p>
+                    <p><strong>Horário:</strong> $horario</p>
+                    <p><strong>Valor:</strong> $valor</p>
+                    <p><strong>$nome_user_ofertante</strong>   $avaliacao</p>
                 </div>
                 
             </div>
@@ -171,14 +108,14 @@ $caminho = 'http://localhost/BicoJobs/';
                         <div class='img'>
                             <img src='$caminho/media/area-atuação/limpeza.svg' alt=''>
                         </div>
-                        <h3>Willian Rodrigues</h3>
+                        <h3>$nome_comp_ofertante</h3>
                         <p>4.0</p>
                     </div>
                     <div class='oferta'>
-                        <p><strong>Serviço: </strong>Encanador</p>
-                        <p><strong>Horário: </strong>Tarde</p>
-                        <p><strong>Descrição: </strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores inventore voluptatum eius maiores nulla nesciunt blanditiis libero similique voluptates deserunt fugit quidem explicabo architecto, dicta quo magni repellendus aspernatur cum!</p>
-                        <p><strong>Valor: </strong>A combinar</p>
+                        <p><strong>Serviço: </strong>$nome</p>
+                        <p><strong>Horário: </strong>$horario</p>
+                        <p><strong>Descrição: </strong>$descricao</p>
+                        <p><strong>Valor: </strong>$valor</p>
                         <p><strong>Contato: </strong>(88) 99999-9999</p>
                     </div>
                 </div>
