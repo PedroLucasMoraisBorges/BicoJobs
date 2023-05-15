68a6c380ef68e0d3591e6bcf57651ad2949347ec
Lucas-Rept <borgeslucas19000@gmail.com>
2023-05-15 19:59:57 -0300

    main main

--- templates/seus_bicos.php
+++ templates/seus_bicos.php
@@ -1,5 +1,6 @@
 <?php 
 session_start();
+@include("../conection/conection.php");
 $caminho = 'http://localhost/BicoJobs/';
 ?>
 
@@ -205,64 +206,74 @@ $caminho = 'http://localhost/BicoJobs/';
 
 
     <div class="conteudo">
-            <div class="geral">
-                <div class="card">
-                    <img src="<?php echo $caminho."media/limp.svg"?>" alt="#" class="img_fundo">
+        <div class="geral">
+        <?php
+        $user_id = $_SESSION['id'];
+        $sql = "SELECT * FROM servico WHERE id_usuario = '$user_id'";
 
-                    <img src="<?php echo $caminho."media/fundo_azul.svg"?>" alt="" class="fundo_azul">
+        $result = $mysqli->query($sql);
 
-                    <div class="card_detalhes">
+        if($result->num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                echo "<div class='card'>
+            <img src='$caminho/media/limp.svg' alt='#' class='img_fundo'>
+
+            <img src='$caminho/media/fundo_azul.svg' alt='' class='fundo_azul'>
+
+            <div class='card_detalhes'>
 
 
-                        <div class="info_princ">
-                            <img src="<?php echo $caminho."media/area-atuação/limpeza.svg"?>" alt="">
-                            <h2>Faxina</h2>
+                <div class='info_princ'>
+                    <img src='$caminho/media/area-atuação/limpeza.svg' alt=''>
+                    <h2>Faxina</h2>
+                </div>
+                
+
+                <div class='info_sec'>
+                    <p><strong>Horário:</strong> Manhã/Tarde</p>
+                    <p><strong>Valor:</strong> A combinar</p>
+                    <p><strong>Pedro</strong> 3.0</p>
+                </div>
+                
+            </div>
+
+            <div class='botao_abrir' onclick='verOferta()'>
+                <p>Abrir</p>
+            </div>
+
+            
+            <div class='modal_verOferta none'>
+                <div class='modal_header'>
+                    <h2>Detalhes da oferta</h2>
+                </div>
+                <div class='oferta_detalhes'>
+                    <div class='pessoais'>
+                        <div class='img'>
+                            <img src='$caminho/media/area-atuação/limpeza.svg' alt=''>
                         </div>
-                        
-
-                        <div class="info_sec">
-                            <p><strong>Horário:</strong> Manhã/Tarde</p>
-                            <p><strong>Valor:</strong> A combinar</p>
-                            <p><strong>Pedro</strong> 3.0</p>
-                        </div>
-                        
+                        <h3>Willian Rodrigues</h3>
+                        <p>4.0</p>
                     </div>
-
-                    <div class="botao_abrir" onclick="verOferta()">
-                        <p>Abrir</p>
-                    </div>
-
-                    
-                    <div class="modal_verOferta none">
-                        <div class="modal_header">
-                            <h2>Detalhes da oferta</h2>
-                        </div>
-                        <div class="oferta_detalhes">
-                            <div class="pessoais">
-                                <div class="img">
-                                    <img src="<?php echo $caminho."media/area-atuação/limpeza.svg"?>" alt="">
-                                </div>
-                                <h3>Willian Rodrigues</h3>
-                                <p>4.0</p>
-                            </div>
-                            <div class="oferta">
-                                <p><strong>Serviço: </strong>Encanador</p>
-                                <p><strong>Horário: </strong>Tarde</p>
-                                <p><strong>Descrição: </strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores inventore voluptatum eius maiores nulla nesciunt blanditiis libero similique voluptates deserunt fugit quidem explicabo architecto, dicta quo magni repellendus aspernatur cum!</p>
-                                <p><strong>Valor: </strong>A combinar</p>
-                                <p><strong>Contato: </strong>(88) 99999-9999</p>
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="modal_footer">
-                            <button class="fechar" onclick="fecharModal()">
-                                Fechar
-                            </button>
-                        </div>
+                    <div class='oferta'>
+                        <p><strong>Serviço: </strong>Encanador</p>
+                        <p><strong>Horário: </strong>Tarde</p>
+                        <p><strong>Descrição: </strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores inventore voluptatum eius maiores nulla nesciunt blanditiis libero similique voluptates deserunt fugit quidem explicabo architecto, dicta quo magni repellendus aspernatur cum!</p>
+                        <p><strong>Valor: </strong>A combinar</p>
+                        <p><strong>Contato: </strong>(88) 99999-9999</p>
                     </div>
                 </div>
+                <hr>
+                <div class='modal_footer'>
+                    <button class='fechar' onclick='fecharModal()'>
+                        Fechar
+                    </button>
+                </div>
             </div>
+        </div>";
+            }
+        }?>
         </div>
+    </div>
         
     </main>
     
