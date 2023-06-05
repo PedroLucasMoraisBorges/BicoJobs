1da16bf906953bc246b152a1582d66d3858a335d
Pedro Lucas <pedrolucasmoraisborges@gmail.com>
2023-06-05 20:36:46 -0300

    PDF

---
+++ Documentos/BD/bicojobs_fisico.sql
@@ -0,0 +1,337 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 06/06/2023 às 00:42
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bicojobs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL,
+  `categoria` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cidade`
+--
+
+CREATE TABLE `cidade` (
+  `id` int(11) NOT NULL,
+  `cep` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `contato`
+--
+
+CREATE TABLE `contato` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `telefone` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `habilidades`
+--
+
+CREATE TABLE `habilidades` (
+  `id` int(11) NOT NULL,
+  `habilidade` varchar(40) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `idioma`
+--
+
+CREATE TABLE `idioma` (
+  `id` int(11) NOT NULL,
+  `idioma` varchar(15) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `notas`
+--
+
+CREATE TABLE `notas` (
+  `id` int(11) NOT NULL,
+  `notas` decimal(2,1) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `dt` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `servico`
+--
+
+CREATE TABLE `servico` (
+  `id` int(11) NOT NULL,
+  `id_categoria` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_cidade` int(11) DEFAULT NULL,
+  `nome` varchar(40) DEFAULT NULL,
+  `valor` decimal(6,2) DEFAULT NULL,
+  `descricao` varchar(350) DEFAULT NULL,
+  `estado` int(11) DEFAULT NULL,
+  `horario` varchar(20) DEFAULT NULL,
+  `img_servico` varchar(50) DEFAULT NULL,
+  `contato` varchar(55) DEFAULT NULL,
+  `serv_status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `servicoavaliar`
+--
+
+CREATE TABLE `servicoavaliar` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_servico` int(11) DEFAULT NULL,
+  `id_categoria` int(11) DEFAULT NULL,
+  `nome` varchar(40) DEFAULT NULL,
+  `valor` decimal(6,2) DEFAULT NULL,
+  `descricao` varchar(350) DEFAULT NULL,
+  `horario` varchar(20) DEFAULT NULL,
+  `img_servico` varchar(50) DEFAULT NULL,
+  `contato` varchar(55) DEFAULT NULL,
+  `id_ofertante` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `servicosfeitos`
+--
+
+CREATE TABLE `servicosfeitos` (
+  `id` int(11) NOT NULL,
+  `valor` decimal(6,2) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `dt` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `id_contato` int(11) DEFAULT NULL,
+  `id_idioma` int(11) DEFAULT NULL,
+  `id_cidade` int(11) DEFAULT NULL,
+  `dt_nascimento` date DEFAULT NULL,
+  `habilidades` varchar(35) DEFAULT NULL,
+  `cpf` char(11) DEFAULT NULL,
+  `tipo_usuario` int(11) DEFAULT NULL,
+  `descricao` varchar(350) DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `senha` varchar(20) DEFAULT NULL,
+  `img_perfil` varchar(100) DEFAULT NULL,
+  `nome_comp` varchar(75) DEFAULT NULL,
+  `quant_servicos` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `cidade`
+--
+ALTER TABLE `cidade`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Índices de tabela `contato`
+--
+ALTER TABLE `contato`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `habilidades`
+--
+ALTER TABLE `habilidades`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Índices de tabela `idioma`
+--
+ALTER TABLE `idioma`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Índices de tabela `notas`
+--
+ALTER TABLE `notas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Índices de tabela `servico`
+--
+ALTER TABLE `servico`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_categoria` (`id_categoria`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_cidade` (`id_cidade`);
+
+--
+-- Índices de tabela `servicoavaliar`
+--
+ALTER TABLE `servicoavaliar`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_servico` (`id_servico`),
+  ADD KEY `id_ofertante` (`id_ofertante`);
+
+--
+-- Índices de tabela `servicosfeitos`
+--
+ALTER TABLE `servicosfeitos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_contato` (`id_contato`),
+  ADD KEY `id_idioma` (`id_idioma`),
+  ADD KEY `id_cidade` (`id_cidade`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `habilidades`
+--
+ALTER TABLE `habilidades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `notas`
+--
+ALTER TABLE `notas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT de tabela `servico`
+--
+ALTER TABLE `servico`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+
+--
+-- AUTO_INCREMENT de tabela `servicoavaliar`
+--
+ALTER TABLE `servicoavaliar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+
+--
+-- AUTO_INCREMENT de tabela `servicosfeitos`
+--
+ALTER TABLE `servicosfeitos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `habilidades`
+--
+ALTER TABLE `habilidades`
+  ADD CONSTRAINT `habilidades_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Restrições para tabelas `idioma`
+--
+ALTER TABLE `idioma`
+  ADD CONSTRAINT `idioma_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Restrições para tabelas `notas`
+--
+ALTER TABLE `notas`
+  ADD CONSTRAINT `notas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Restrições para tabelas `servico`
+--
+ALTER TABLE `servico`
+  ADD CONSTRAINT `servico_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id`),
+  ADD CONSTRAINT `servico_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`),
+  ADD CONSTRAINT `servico_ibfk_3` FOREIGN KEY (`id_cidade`) REFERENCES `cidade` (`id`);
+
+--
+-- Restrições para tabelas `servicoavaliar`
+--
+ALTER TABLE `servicoavaliar`
+  ADD CONSTRAINT `servicoavaliar_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`),
+  ADD CONSTRAINT `servicoavaliar_ibfk_2` FOREIGN KEY (`id_servico`) REFERENCES `servico` (`id`),
+  ADD CONSTRAINT `servicoavaliar_ibfk_3` FOREIGN KEY (`id_ofertante`) REFERENCES `usuario` (`id`);
+
+--
+-- Restrições para tabelas `servicosfeitos`
+--
+ALTER TABLE `servicosfeitos`
+  ADD CONSTRAINT `servicosfeitos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Restrições para tabelas `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_2` FOREIGN KEY (`id_idioma`) REFERENCES `idioma` (`id`),
+  ADD CONSTRAINT `usuario_ibfk_3` FOREIGN KEY (`id_cidade`) REFERENCES `cidade` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
